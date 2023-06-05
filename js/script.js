a500befa6796adc5dc06b6d8487c8e5b9209500f
Olha Ovcharenko <ovcharenko.olga.2014@gmail.com>
2023-06-05 20:43:21 +0300

    change script

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
-
-printMessage('Jeśli Twój ruch to kamien, to przegraleś!')
-
 let palyerMove = `papier`;
 
 printMessage('Zagrałem ' + palyerMove + '! Jeśli Twój ruch to kamien, to przegraleś!');
